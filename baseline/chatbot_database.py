d11b2f4caf6a40d8abd450eec26e956bb928b4d3
Shroomi <dingmengru1993@gmail.com>
2018-10-08 13:53:38 +0200

    create database for comment-reply

--- baseline/chatbot_database.py
+++ baseline/chatbot_database.py
@@ -2,3 +2,8 @@ import sqlite3
 import json
 from datetime import datetime
 
+timeframe = '2015-05'
+sql_transaction = []
+
+connection = sqlite3.connect('{}.db'.format(timeframe))
+c = connection.cursor()
